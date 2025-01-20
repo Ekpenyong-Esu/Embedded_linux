2f4b6c93891b6bd7372c3bed93bf5d94b003ea0f
Ekpenyong-Esu <eeg21003@student.mdu.se>
2025-01-20 21:30:48 +0100

    adding init file for stop and start to assignment 3

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket server daemon"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket server daemon"
+        start-stop-daemon -K -n aesdsocket -s TERM
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+esac
+
+exit 0
